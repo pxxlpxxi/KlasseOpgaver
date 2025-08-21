40dcd852ba03b3aa969a9664123f9a11c7b40bc0
pxxlpxxi <vaikeflyger@hotmail.com>
2025-08-21 07:59:09 +0200

    + box + oprydning

--- Box.cs
+++ Box.cs
@@ -13,24 +13,8 @@ namespace KlasseOpgaver
         mellem=2,
         stor=3,
     }
-    //public enum Type( boxType) { 
-    //    lille,
-    //    mellem, 
-    //    stor
-    
-    //}
     public class Box : IComparable<Box>
     {
-        //private readonly double _højde;
-        //private readonly double _bredde;
-        //private double _længde;
-        ////public double Højde => _højde;
-        ////public double Bredde => _bredde;
-        ////public double Længde => _længde;
-
-        //private readonly boxType _type;
-        //public boxType Type => _type;
-
         public double Højde { get; set; }
         public boxType Type { get; set; }
         public double Bredde { get; set; }
@@ -45,16 +29,10 @@ namespace KlasseOpgaver
             Bredde = bredde;
             Længde= længde;
             Type = type;
-
-
-            //if (boxtype == "lille") { BoxType = BoxType.lille; }
-            //else if (boxtype == "mellem") { BoxType = BoxType.mellem; }
-            //else { BoxType = BoxType.stor; }
         }
         public override string ToString()
         {
             string retur = $"Højde: {Højde}\nBredde: {Bredde}\nLængde: {Længde}\n{Type}";
-            //base.ToString(); 
             return retur;
         }
         public double GetVolume() {
@@ -83,22 +61,6 @@ namespace KlasseOpgaver
 
             return newBox;
         }
-        //public static Box operator +(Box left, Box right)
-        //{
-        //    return
-        //        }
-        //public static Box Create(BoxType boxType) { 
-        //return new Box(boxType);
-        //}
-        //    public static void BoxCalculator(Box(Boxtye) box, Box box2)
-        //    {
-        //        BoxType boxt2 = box2.BoxType;
-
-        //        Enum box3 = box.BoxType+box2.BoxType;
-        //        Box C=new(boxt1+boxt2);
-        //        return new Box(Box.BoxType + box2.BoxType);
-        //    }
-        //}
     }
 }
 
